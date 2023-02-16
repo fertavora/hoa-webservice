2528502c9f63be39e3a293fac85593f7e835f7d1
Fernando Távora <fertavora@tutamail.com>
2023-02-16 12:07:46 +0100

    added morgan to log responses

--- database/index.ts
+++ database/index.ts
@@ -8,8 +8,6 @@ const user:any = process.env.DB_USER;
 const password:any = process.env.DB_PASSWORD;
 const dbname:any = process.env.DB_NAME;
 
-console.log(`password is ${password}`);
-
 const sequelize = new Sequelize(dbname, user, password, {
   dialect: 'postgres',
   logging: false
