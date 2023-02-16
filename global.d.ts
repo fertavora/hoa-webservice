1907b6ca06b57eb4758ba2359566b956968fe0b7
Fernando Távora <fertavora@tutamail.com>
2023-02-16 11:49:41 +0100

    change database to postgres

--- global.d.ts
+++ global.d.ts
@@ -3,5 +3,9 @@ namespace NodeJS {
     API_PORT: number;
     API_HOST: string;
     JWT_SECRET: string;
+    DB_HOST: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_NAME: string;
   }
 }
--- server.ts
+++ server.ts
@@ -14,12 +14,8 @@ app.listen(port, host, () => {
   console.info(`Express server is started at ${host}:${port}.`);
 });
 
-
-// reference: https://github.com/ipenywis/express-login-register-api
-// todo change to postgre docker db https://github.com/docker-library/docs/blob/master/postgres/README.md
 // todo review loggin of db
 // todo add morgan or other loggin https://www.npmjs.com/package/morgan
 // todo review cors and helmet
 // todo create heroku account and deploy
 // todo models for consorcio
-// todo rename and sync repo
