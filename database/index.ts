070922b0bc90a859db5b9162c7382b82138ef18b
Fernando Távora <fertavora@tutamail.com>
2023-02-20 19:55:08 +0100

    added cors, helmet and node_env handling

--- database/index.ts
+++ database/index.ts
@@ -1,18 +1,24 @@
 import dotenv from 'dotenv'; 
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
 dotenv.config();
-
 const hostname:any = process.env.DB_HOST;
 const user:any = process.env.DB_USER;
 const password:any = process.env.DB_PASSWORD;
 const dbname:any = process.env.DB_NAME;
+const environment:any = process.env.NODE_ENV;
 
-const sequelize = new Sequelize(dbname, user, password, {
-  dialect: 'postgres',
-  logging: false
-});
+let sequelize:typeof Sequelize;
 
+const setEnvironment:any = {
+  development: () => new Sequelize('sqlite::memory:'),
+  production: () => new Sequelize(dbname, user, password, {
+    dialect: 'postgres',
+    logging: false
+  })
+}
+
+sequelize = setEnvironment[environment]();
 sequelize.sync();
 
 export default sequelize;
