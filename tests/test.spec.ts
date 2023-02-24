f4959a2a91c4cd2d24b455f0c3ee8b0963c84869
Fernando Távora <fertavora@tutamail.com>
2023-02-24 13:12:54 +0100

    invalid login test added

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -6,6 +6,18 @@ import app from '../app';
 
 describe('HOA Webservice', () => {
   let access_token:string;
+  let newUser:any;
+
+  before(() => {
+    newUser = {
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+    }
+  });
+
   it('Healthcheck', async () => {
     const response = await request(app)
       .get('/api/v1/ping');
@@ -14,13 +26,6 @@ describe('HOA Webservice', () => {
   });
 
   it('Register new user', async () => {
-    const newUser = {
-      username: faker.internet.userName(),
-      password: faker.internet.password(),
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.email(),
-    }
     const response = await request(app)
         .post('/api/v1/register')
         .send(newUser);
@@ -29,24 +34,31 @@ describe('HOA Webservice', () => {
       expect(response.body.message).to.equal('Thanks for registering');
   });
 
-  it.skip('Login in', async () => {
+  it('Invalid login in', async () => {
     const response = await request(app)
       .post('/api/v1/login')
       .send({ "username": "admin", "password": "nimda"});
-    expect(response.headers["content-type"]).to.includes('application/json');
+    expect(response.status).to.equal(401);
+    expect(response.text).to.equal('Unauthorized');
+  });
+
+  it('Valid login in', async () => {
+    const response = await request(app)
+      .post('/api/v1/login')
+      .send({ "username": newUser.username, "password": newUser.password});
     expect(response.status).to.equal(200);
-    expect(response.body.message).to.equal('Welcome admin!');
+    expect(response.headers["content-type"]).to.includes('application/json');
+    expect(response.body.message).to.equal(`Welcome ${newUser.username}!`);
     expect(response.body.access_token).to.not.be.null;
     access_token = response.body.access_token;
   });
 
-  it.skip('Read data', async () => {
+  it('Read data', async () => {
     const response = await request(app)
       .get('/api/v1/data')
-      .set('Authorization', `Bearer ${access_token}`)
-      .send({ "username": "admin", "password": "nimda"});
-    expect(response.headers["content-type"]).to.includes('application/json');
+      .set('Authorization', `Bearer ${access_token}`);
     expect(response.status).to.equal(200);
+    expect(response.headers["content-type"]).to.includes('application/json');
     expect(response.body.message).to.equal('Get data');
   });
 });
